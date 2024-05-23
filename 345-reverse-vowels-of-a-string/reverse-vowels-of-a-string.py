bdc99e6862fda6ae4497da0fd49ed25553e1e1ef
Yanlin Chen <32689753+Ychen463@users.noreply.github.com>
2024-05-23 15:27:41 -0400

    Update reverse-vowels-of-a-string.py

--- 345-reverse-vowels-of-a-string/reverse-vowels-of-a-string.py
+++ 345-reverse-vowels-of-a-string/reverse-vowels-of-a-string.py
@@ -9,14 +9,19 @@ class Solution(object):
         p1, p2 = 0, len(s)-1
         
         while p1 < p2:
+            # 找到左边的第一个元音字符
             while p1 < p2 and s_list[p1] not in vowels:
                 p1 +=1
+            # 找到右边的第一个元音字符
             while p1 < p2 and s_list[p2] not in vowels:
                 p2 -=1
+            # 交换两个元音字符
             if p1 < p2:
                 s_list[p1], s_list[p2] = s_list[p2], s_list[p1]
                 p1 += 1
                 p2 -= 1
         return "".join(s_list)
+
+        
 
         
