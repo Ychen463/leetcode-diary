c81c8bd41dd05aadf4ffc0b37edc2aed319fca03
Yanlin Chen <32689753+Ychen463@users.noreply.github.com>
2024-05-23 16:26:00 -0400

    Time: 288 ms (51.24%) | Memory: 18.5 MB (89.72%) - LeetSync

--- 238-product-of-array-except-self/product-of-array-except-self.py
+++ 238-product-of-array-except-self/product-of-array-except-self.py
@@ -4,26 +4,32 @@ class Solution(object):
         :type nums: List[int]
         :rtype: List[int]
         """
-        # nums = [1,2,3,4]
-        # right_nums = [24,12,4,1]
-        # left_nums = [1,1,2,6]
-        
-        # res = right * left
 
-        p1, p2 = 1, len(nums)-2
-        left_prd = [0]*len(nums)
-        right_prd = [0]*len(nums)
-        left_prd[0] = 1
-        right_prd[len(nums)-1] = 1
-        res = []
+        # p1, p2 = 1, len(nums)-2
+        # left_prd = [0]*len(nums)
+        # right_prd = [0]*len(nums)
+        # left_prd[0] = 1
+        # right_prd[len(nums)-1] = 1
+        # res = []
 
-        while p1 < len(nums) :
-            left_prd[p1] = nums[p1-1]*left_prd[p1-1]
-            right_prd[p2] = nums[p2+1]*right_prd[p2+1]
-            p1 +=1
-            p2 -=1
-        for i in range(len(left_prd)):
-            res.append(left_prd[i] * right_prd[i])
-        print(res)
+        # while p1 < len(nums) :
+        #     left_prd[p1] = nums[p1-1]*left_prd[p1-1]
+        #     right_prd[p2] = nums[p2+1]*right_prd[p2+1]
+        #     p1 +=1
+        #     p2 -=1
+        # for i in range(len(left_prd)):
+        #     res.append(left_prd[i] * right_prd[i])
+        # print(res)
+        # return res
+        n = len(nums)
+        res = [1] * n
+
+        for i in range(1,len(nums)):
+            res[i] = nums[i-1]*res[i-1]
+        right_product = 1
+        for i in range(len(nums)-1,-1,-1):
+            res[i] = right_product * res[i]
+            right_product *= nums[i]
         return res
+
         
