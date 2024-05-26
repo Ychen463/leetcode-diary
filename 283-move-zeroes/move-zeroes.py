946e2039e7b5a16a672dece35e1218f1d48237ac
Yanlin Chen <32689753+Ychen463@users.noreply.github.com>
2024-05-26 17:16:18 -0400

    Time: 108 ms (91.05%) | Memory: 12.7 MB (89.47%) - LeetSync

--- 283-move-zeroes/move-zeroes.py
+++ 283-move-zeroes/move-zeroes.py
@@ -4,13 +4,10 @@ class Solution(object):
         :type nums: List[int]
         :rtype: None Do not return anything, modify nums in-place instead.
         """
-        write = 0 
-        if len(nums) <=1:
-            return nums
+        write = 0
         for read in range(len(nums)):
             if nums[read] != 0:
                 nums[write] = nums[read]
-                write +=1 
-        for i in range(write, len(nums)):
-            nums[i] = 0
-        
+                write +=1
+        for write in range(write, len(nums)):
+            nums[write] =0
