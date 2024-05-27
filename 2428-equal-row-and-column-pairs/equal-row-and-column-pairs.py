fdec9df073ec0747522d627a7f424bc3afb0149f
Yanlin Chen <32689753+Ychen463@users.noreply.github.com>
2024-05-27 06:47:49 -0400

    Time: 370 ms (90.54%) | Memory: 21.3 MB (75.38%) - LeetSync

--- 2428-equal-row-and-column-pairs/equal-row-and-column-pairs.py
+++ 2428-equal-row-and-column-pairs/equal-row-and-column-pairs.py
@@ -1,18 +1,30 @@
 class Solution:
     def equalPairs(self, grid: List[List[int]]) -> int:
+        # rows, columns = len(grid), len(grid[0])
+        # hashmap_row = {}
+        # hashmap_col = {}
+        # for i in range(rows):
+        #     row_tuple = tuple(grid[i])
+        #     hashmap_row[row_tuple] = hashmap_row.get(row_tuple, 0) + 1
+        # for j in range(columns):
+        #     col_tuple = tuple(grid[i][j] for i in range(rows))
+        #     hashmap_col[col_tuple] = hashmap_col.get(col_tuple, 0) + 1
+
+        # counter = 0
+        # for rk, rv in hashmap_row.items():
+        #     if rk in hashmap_col.keys():
+        #         counter += rv * hashmap_col.get(rk)
+        # return counter
+        
+        
         rows, columns = len(grid), len(grid[0])
+        counter = 0
         hashmap_row = {}
-        hashmap_col = {}
         for i in range(rows):
             row_tuple = tuple(grid[i])
             hashmap_row[row_tuple] = hashmap_row.get(row_tuple, 0) + 1
-            # i = 0, j = 0,1,2
+
         for j in range(columns):
             col_tuple = tuple(grid[i][j] for i in range(rows))
-            hashmap_col[col_tuple] = hashmap_col.get(col_tuple, 0) + 1
-
-        counter = 0
-        for rk, rv in hashmap_row.items():
-            if rk in hashmap_col.keys():
-                counter += rv * hashmap_col.get(rk)
+            counter += hashmap_row.get (col_tuple,0)
         return counter
