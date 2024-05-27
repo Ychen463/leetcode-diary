5c9a9c68ecd25defd0571db7e9a6db354b6a8fbb
Yanlin Chen <32689753+Ychen463@users.noreply.github.com>
2024-05-27 03:50:56 -0400

    Time: 137 ms (77.17%) | Memory: 16.7 MB (99.55%) - LeetSync

--- 1392-find-the-difference-of-two-arrays/find-the-difference-of-two-arrays.py
+++ 1392-find-the-difference-of-two-arrays/find-the-difference-of-two-arrays.py
@@ -1,6 +1,18 @@
 class Solution:
-    def findDifference(self, nums1: List[int], nums2: List[int]) -> List[List[int]]:
-        res = [[],[]]
-        res[0] = list(set([num for num in nums1 if num not in nums2]))
-        res[1] = list(set([num for num in nums2 if num not in nums1]))
-        return res
+    # Method 1
+    #def findDifference(self, nums1: List[int], nums2: List[int]) -> List[List[int]]:
+        # res = [[],[]]
+            # res[0] = list(set([num for num in nums1 if num not in nums2]))
+            # res[1] = list(set([num for num in nums2 if num not in nums1]))
+            # return res
+    
+    # Method 2： Hashmap
+    def getElementsOnlyInFirstList(self, nums1: List[int], nums2: List[int]) -> List[List[int]]:
+        onlyInNums1 = set()
+        existsNums2 = set(nums2)
+        for num in nums1:
+            if num not in existsNums2:
+                onlyInNums1.add(num)
+        return list(onlyInNums1)
+    def findDifference(self, nums1, nums2):
+        return [self.getElementsOnlyInFirstList(nums1, nums2), self.getElementsOnlyInFirstList(nums2, nums1)]
