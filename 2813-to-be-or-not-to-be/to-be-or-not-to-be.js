904d30eb91282797a74d653a8589bf88432b4213
Yanlin Chen <32689753+Ychen463@users.noreply.github.com>
2024-05-29 15:20:18 -0400

    Time: 41 ms (96.69%) | Memory: 48.8 MB (53.19%) - LeetSync

--- 2813-to-be-or-not-to-be/to-be-or-not-to-be.js
+++ 2813-to-be-or-not-to-be/to-be-or-not-to-be.js
@@ -2,20 +2,41 @@
  * @param {string} val
  * @return {Object}
  */
-var expect = function(val) {
-    return {
-        toBe: (anotherVal) => {
-            if (val !== anotherVal) throw new Error ("Not Equal")  
-            return true;
+// var expect = function(val) {
+//     return {
+//         toBe: (anotherVal) => {
+//             if (val !== anotherVal) throw new Error ("Not Equal")  
+//             return true;
              
-        },
-        notToBe: (anotherVal)=> {
-            if (val === anotherVal) throw new Error ("Equal")  
-            return true; 
-        }
+//         },
+//         notToBe: (anotherVal)=> {
+//             if (val === anotherVal) throw new Error ("Equal")  
+//             return true; 
+//         }
 
+//     }
+// };
+
+class Expect{
+    constructor(val){
+        this.val = val;
+    };
+    toBe(anotherVal){
+        if (this.val === anotherVal){
+            return true
+        } throw new Error("Not Equal")            
+    };
+    notToBe(anotherVal){
+        if (this.val !== anotherVal){
+            return true
+        } throw new Error("Equal")        
     }
-};
+}
+const expect = (val) => new Expect(val);
+
+// function expect(val) {
+//     return new Expect(val);
+// }
 
 /**
  * expect(5).toBe(5); // true
