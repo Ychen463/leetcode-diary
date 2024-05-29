33f6dc05108826e23a9a936b59ec6ba3c15d9fb9
Yanlin Chen <32689753+Ychen463@users.noreply.github.com>
2024-05-29 16:26:22 -0400

    Time: 58 ms (31.31%) | Memory: 49 MB (76.95%) - LeetSync

--- 2761-array-reduce-transformation/array-reduce-transformation.js
+++ 2761-array-reduce-transformation/array-reduce-transformation.js
@@ -6,7 +6,7 @@
  */
 var reduce = function(nums, fn, init) {
     var val = init
-    // if (nums.length === 0) return init
+    if (nums.length === 0) return init
     for (let i = 0; i < nums.length; i++){
         val = fn(val, nums[i])
     }
