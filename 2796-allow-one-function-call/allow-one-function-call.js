11125373d88a0234c7c090a78482644efdc54caf
Yanlin Chen <32689753+Ychen463@users.noreply.github.com>
2024-05-29 17:17:32 -0400

    Time: 53 ms (51.52%) | Memory: 47.7 MB (99.51%) - LeetSync

---
+++ 2796-allow-one-function-call/allow-one-function-call.js
@@ -0,0 +1,26 @@
+/**
+ * @param {Function} fn
+ * @return {Function}
+ */
+var once = function(fn) {
+    var calls = 1
+    return function(...args){
+        if (calls <=1){
+            calls++
+            return fn(...args)
+        }
+        else{
+            return undefined
+        }
+        
+        
+    }
+};
+
+/**
+ * let fn = (a,b,c) => (a + b + c)
+ * let onceFn = once(fn)
+ *
+ * onceFn(1,2,3); // 6
+ * onceFn(2,3,6); // returns undefined without calling fn
+ */
