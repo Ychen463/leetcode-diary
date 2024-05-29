e5be9ee5cd38a6cbd753ca88aafd1597f9399893
Yanlin Chen <32689753+Ychen463@users.noreply.github.com>
2024-05-29 16:56:22 -0400

    Time: 65 ms (49.50%) | Memory: 49.4 MB (94.23%) - LeetSync

--- 2741-function-composition/function-composition.js
+++ 2741-function-composition/function-composition.js
@@ -5,11 +5,12 @@
 var compose = function(functions) {
     
     return function(x) {
-        // for (let i = functions.length -1 ; i >=0; i--){
-        //     x = functions[i](x)
-        // }
-        // return x
-        return functions.reduceRight((acc,func) => func(acc), x);
+        let result = x
+        for (let i = functions.length -1 ; i >=0; i--){
+            result = functions[i].call(this, result)
+        }
+        return result
+        // return functions.reduceRight((acc,func) => func(acc), x);
 
     }
 };
