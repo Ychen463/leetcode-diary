2fedca943785d7b23fe87e8085e3db85670e9ca9
Yanlin Chen <32689753+Ychen463@users.noreply.github.com>
2024-05-29 16:01:03 -0400

    Time: 57 ms (23.14%) | Memory: 48.7 MB (57.93%) - LeetSync

--- 2747-apply-transform-over-each-element-in-array/apply-transform-over-each-element-in-array.js
+++ 2747-apply-transform-over-each-element-in-array/apply-transform-over-each-element-in-array.js
@@ -6,7 +6,7 @@
 var map = function(arr, fn) {
     const newArray = []
     for (let i =0; i < arr.length; i++){
-        newArray[i] = fn(arr[i],i)
+        newArray.push(fn(arr[i],i))
     }
     return newArray
 };
