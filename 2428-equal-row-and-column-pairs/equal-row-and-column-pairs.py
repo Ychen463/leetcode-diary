89c5c1e4611c3636b6981d30fa89b9a4fcdd49de
Yanlin Chen <32689753+Ychen463@users.noreply.github.com>
2024-05-29 00:48:45 -0400

    Time: 445 ms (41.52%) | Memory: 29.8 MB (5.09%) - LeetSync

--- 2428-equal-row-and-column-pairs/equal-row-and-column-pairs.py
+++ 2428-equal-row-and-column-pairs/equal-row-and-column-pairs.py
@@ -16,15 +16,50 @@ class Solution:
         #         counter += rv * hashmap_col.get(rk)
         # return counter
         
-        
-        rows, columns = len(grid), len(grid[0])
-        counter = 0
-        hashmap_row = {}
-        for i in range(rows):
-            row_tuple = tuple(grid[i])
-            hashmap_row[row_tuple] = hashmap_row.get(row_tuple, 0) + 1
+        # # Method 2, optimal
+        # rows, columns = len(grid), len(grid[0])
+        # counter = 0
+        # hashmap_row = {}
+        # for i in range(rows):
+        #     row_tuple = tuple(grid[i])
+        #     hashmap_row[row_tuple] = hashmap_row.get(row_tuple, 0) + 1
 
-        for j in range(columns):
-            col_tuple = tuple(grid[i][j] for i in range(rows))
-            counter += hashmap_row.get (col_tuple,0)
-        return counter
+        # for j in range(columns):
+        #     col_tuple = tuple(grid[i][j] for i in range(rows))
+        #     counter += hashmap_row.get(col_tuple,0)
+        # return counter
+
+        # Method 3 Prefix-Trie
+        count = 0
+        trie = Trie()
+
+        for row in grid:
+            trie.insert(row)
+        for j in range(len(grid)):
+            each_col = [grid[i][j] for i in range(len(grid))]
+            count += trie.search(each_col)
+        return count
+class TrieNode:
+    def __init__(self):
+        self.count = 0
+        self.children = {}
+class Trie:
+    def __init__(self):
+        self.trie = TrieNode()
+    def insert(self, array):
+        root = self.trie
+        for a in array:
+            if a not in root.children:
+                root.children[a] = TrieNode()
+            root = root.children[a]
+        root.count +=1
+    def search(self, array):
+        root = self.trie
+        for a in array:
+            if a in root.children:
+                root = root.children[a]
+            else:
+                return 0
+        return root.count
+        
+        
