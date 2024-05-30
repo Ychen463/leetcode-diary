3a28f5f74167c537ccf555bea4c8d89cb4c03fbd
Yanlin Chen <32689753+Ychen463@users.noreply.github.com>
2024-05-30 01:54:09 -0400

    Time: 53 ms (38.58%) | Memory: 48.9 MB (24.93%) - LeetSync

--- 2734-array-prototype-last/array-prototype-last.js
+++ 2734-array-prototype-last/array-prototype-last.js
@@ -2,7 +2,10 @@
  * @return {null|boolean|number|string|Array|Object}
  */
 Array.prototype.last = function() {
-    return  this.length>=1  ? this.at(-1) : -1
+    if (this.length === 0) {
+        return -1;
+    }
+    return  this.at(-1)
 };
 
 /**
