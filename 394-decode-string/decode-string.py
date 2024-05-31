fff21151de995053474a96e891cfd64bbcc0b001
Yanlin Chen <32689753+Ychen463@users.noreply.github.com>
2024-05-31 16:31:19 -0400

    Time: 30 ms (84.07%) | Memory: 16.4 MB (99.03%) - LeetSync

--- 394-decode-string/decode-string.py
+++ 394-decode-string/decode-string.py
@@ -1,33 +1,32 @@
 class Solution:
+    # Method 1: Recursion
+    def __init__(self):
+            self.index = 0
     def decodeString(self, s: str) -> str:
-        count_st = []
-        counter = 0
-        result_st = []
-        cur_str = []
-        for char in s:
-            if char.isdigit():
-                counter = counter * 10 + int(char)
-            elif char == '[':
-                count_st.append(counter)
-                result_st.append(cur_str)
-                counter = 0
-                cur_str = []
-            elif char == ']':
-                cur_count = count_st.pop()
-                last_str = result_st.pop()
-                cur_str = last_str + cur_count * cur_str
+        # --------------------------------
+        # Method 1: Recursion
+        result = []
+        while self.index < len(s) and s[self.index] != ']':
+            # 1. 如果当前字符是字母，直接添加到 result 列表中，并更新索引 index。
+            if not s[self.index].isdigit(): # char 
+                result.append(s[self.index])
+                self.index +=1
             else:
-                cur_str.append(char)
-        return ''.join(cur_str)
+                # 2. 如果当前字符是数字，构建重复次数 k，处理多位数字。
+                k = 0 # 重复次数
+                while self.index <len(s) and s[self.index].isdigit():
+                    k = k * 10 + int(s[self.index])
+                    self.index +=1
+                
+                # 3. 
+                self.index += 1  # 跳过 '['
+                decoded_string = self.decodeString(s)  # 递归处理子字符串
+                self.index += 1  # 跳过 ']'
+                result.append(decoded_string * k) # 将解码结果重复 k 次并添加到 result 中
+        return ''.join(result)
 
-
-
-
-
-
-
-
-
+        # --------------------------------
+        # Method 2: Stack
         # counter = 0
         # result_st = [] # 用于存储部分结果字符串
         # count_st = [] # 用于存储重复次数
