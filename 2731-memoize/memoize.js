9d985d05b5dc036e1fd03f2c8f53b41587372638
Yanlin Chen <32689753+Ychen463@users.noreply.github.com>
2024-06-01 03:15:21 -0400

    Time: 258 ms (98.34%) | Memory: 89.6 MB (47.59%) - LeetSync

--- 2731-memoize/memoize.js
+++ 2731-memoize/memoize.js
@@ -3,17 +3,30 @@
  * @return {Function}
  */
 function memoize(fn) {
-    const cache = {}; // 声明为局部变量 
-    return function(...args) {
-        const key = JSON.stringify(args);
-        if (key in cache) {
-            return cache[key]
-        } else {
-            cache[key] = fn(...args)
-            return cache[key]
-        }
-        
+    // const cache = new Map(); // 声明为局部变量 
+    // return function(...args) {
+    //     const key = JSON.stringify(args);
+    //     if (key in cache) {
+    //         return cache[key]
+    //     }
+    //     cache[key] = fn(...args)
+    //     return cache[key]
+    //     }
+    // }
+    const cache = new Map();
+  return function() {
+    let key = arguments[0];
+    if (arguments[1]) {
+      key += arguments[1] * 100001;
     }
+    const result = cache.get(key);
+    if (result !== undefined) {
+      return result;
+    }
+    const functionOutput = fn.apply(null, arguments);
+    cache.set(key, functionOutput);
+    return functionOutput;
+  }
 }
 
 
