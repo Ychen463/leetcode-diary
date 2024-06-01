b01df120222551b02d0126f63dbddc7b2bf2a650
Yanlin Chen <32689753+Ychen463@users.noreply.github.com>
2024-06-01 18:38:01 -0400

    Time: 60 ms (71.73%) | Memory: 49.3 MB (79.25%) - LeetSync

--- 2859-add-two-promises/add-two-promises.js
+++ 2859-add-two-promises/add-two-promises.js
@@ -15,9 +15,22 @@ var addTwoPromises = async function(promise1, promise2) {
     // return Sum(arrayNum)
     // --------------------------------
     // Method 2: Using only Await
+    //  Note: Use of await directly on promise1 and promise2 
+    // is not as concurrent as using Promise.all(). 
+    // As awaiting promises one after the other will 
+    // result in a longer total time for resolution 
+    // compared to awaiting them concurrently with Promise.all().
+    // try{
+    //     return await promise1 + await promise2
+    // }catch (error) {
+    //     console.error(error);
+    //     throw new Error(error);
+    // }
+    // --------------------------------
+    // Method 3: Promise.then
     try{
-        return await promise1 + await promise2
-    }catch (error) {
+        return promise1.then((val1)=> promise2.then((val2) => val1+val2))
+    }catch(error){
         console.error(error);
         throw new Error(error);
     }
