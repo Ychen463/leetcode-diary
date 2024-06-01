e3923852bf1230a078f360e1b069c2aebd98ac24
Yanlin Chen <32689753+Ychen463@users.noreply.github.com>
2024-06-01 18:33:48 -0400

    Time: 56 ms (87.17%) | Memory: 49.4 MB (59.16%) - LeetSync

--- 2859-add-two-promises/add-two-promises.js
+++ 2859-add-two-promises/add-two-promises.js
@@ -4,14 +4,23 @@
  * @return {Promise}
  */
 var addTwoPromises = async function(promise1, promise2) {
-    try {
-        const [res1, res2] = await Promise.all([promise1, promise2]);
-        return res1 + res2
-    } catch (error) {
-        throw new Error(error)
-    }
+    // // Method 1: Promise.all()
+    // try {
+    //     const [res1, res2] = await Promise.all([promise1, promise2]);
+    //     return res1 + res2
+    // } catch (error) {
+    //     throw new Error(error)
+    // }
     
-    return Sum(arrayNum)
+    // return Sum(arrayNum)
+    // --------------------------------
+    // Method 2: Using only Await
+    try{
+        return await promise1 + await promise2
+    }catch (error) {
+        console.error(error);
+        throw new Error(error);
+    }
 };
 
 /**
