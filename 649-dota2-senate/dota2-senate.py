3f7687e6e4b8f24be59bfabc4011deba7bdbd58b
Yanlin Chen <32689753+Ychen463@users.noreply.github.com>
2024-06-01 16:41:38 -0400

    Time: 1025 ms (7.59%) | Memory: 16.8 MB (73.27%) - LeetSync

--- 649-dota2-senate/dota2-senate.py
+++ 649-dota2-senate/dota2-senate.py
@@ -29,25 +29,62 @@ class Solution:
         # # 判断最终胜利者
         # return "Radiant" if q_r else "Dire"
                 
-        # Method 2: Single Queue
-        r_count, d_count = senate.count('R') , senate.count('D')
-        floating_ban_r = floating_ban_d = 0
-        q = deque(senate)
-        while r_count and d_count:
-            cur = q.popleft()
-            if cur == 'D':
-                if floating_ban_r: # D 本轮 被banned
-                    floating_ban_r -= 1
-                    d_count -=1
-                else:
-                    floating_ban_d +=1 # 否则 D senator会ban R
-                    q.append(cur) # D 行驶ban权利，排队下一轮
-            else:
-                if floating_ban_d: # D 本轮 被banned
-                    floating_ban_d -= 1
-                    r_count -=1
-                else:
-                    floating_ban_r +=1 # 否则 D senator会ban R
-                    q.append(cur) # D 行驶ban权利，排队下一轮
+        # # Method 2: Single Queue
+        # r_count, d_count = senate.count('R') , senate.count('D')
+        # floating_ban_r = floating_ban_d = 0
+        # q = deque(senate)
+        # while r_count and d_count:
+        #     cur = q.popleft()
+        #     if cur == 'D':
+        #         if floating_ban_r: # D 本轮 被banned
+        #             floating_ban_r -= 1
+        #             d_count -=1
+        #         else:
+        #             floating_ban_d +=1 # 否则 D senator会ban R
+        #             q.append(cur) # D 行驶ban权利，排队下一轮
+        #     else:
+        #         if floating_ban_d: # D 本轮 被banned
+        #             floating_ban_d -= 1
+        #             r_count -=1
+        #         else:
+        #             floating_ban_r +=1 # 否则 D senator会ban R
+        #             q.append(cur) # D 行驶ban权利，排队下一轮
 
+        # return 'Dire' if d_count else 'Radiant'
+
+        # Method 3 Greedy Algorithm
+        senate = list(senate) # 换成list因为可以进行删减
+        d_count = senate.count('D')
+        r_count = len(senate) - d_count
+        
+        # 定义禁令函数，从指定位置开始寻找并禁止对方参议员
+        def ban(to_ban, start_index):
+            # loop_around的作用是在每次指针移动时检查是否回到了列表的开头。
+            # 如果指针已经回到了列表的开头，说明我们已经遍历了一轮。
+            # 这样做是为了确保我们能够正确地找到并禁掉一个对方参议员。
+            loop_around = False 
+            pointer = start_index
+            while True: # 用while是为了找到 start_index后的 第一个看到的敌人
+                if pointer == 0:
+                    loop_around = True
+                if senate[pointer] == to_ban:
+                    senate.pop(pointer)
+                    break
+                pointer = (pointer+1)% len(senate)
+            return loop_around
+
+
+
+        turn = 0
+        while d_count and r_count:
+            # ban_senator_before 的意思是刚好轮换了一轮
+            if senate[turn] == 'D':
+                ban_senator_before = ban('R', (turn + 1 ) % len(senate))
+                r_count -= 1
+            elif senate[turn] == 'R':
+                ban_senator_before = ban('D', (turn + 1 ) % len(senate))
+                d_count -= 1
+            if ban_senator_before:
+                turn -= 1
+            turn = (turn+1) % len(senate)
         return 'Dire' if d_count else 'Radiant'
