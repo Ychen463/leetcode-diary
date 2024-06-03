7993cbf26c28d52fa1594c03929f918b35dffea9
Yanlin Chen <32689753+Ychen463@users.noreply.github.com>
2024-06-03 16:08:47 -0400

    Time: 64 ms (53.98%) | Memory: 17.6 MB (28.78%) - LeetSync

--- 2572-append-characters-to-string-to-make-subsequence/append-characters-to-string-to-make-subsequence.py
+++ 2572-append-characters-to-string-to-make-subsequence/append-characters-to-string-to-make-subsequence.py
@@ -1,12 +1,11 @@
 class Solution:
     def appendCharacters(self, s: str, t: str) -> int:
         pointer_s = pointer_t = 0
-        counter = len(t)
+        # 遍历s和t，直到其中一个结束
         while pointer_t <len(t) and pointer_s < len(s):
             if t[pointer_t] == s[pointer_s]:
-                counter -=1
                 pointer_t +=1
             pointer_s +=1
-
-        return counter
+        # 返回剩余未匹配的t的字符数量
+        return len(t) - pointer_t
         
