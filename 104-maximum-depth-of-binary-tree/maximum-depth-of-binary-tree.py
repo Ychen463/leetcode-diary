bf186ce51ce629658a2cff60dfd1f979c4830cec
Yanlin Chen <32689753+Ychen463@users.noreply.github.com>
2024-06-03 20:53:43 -0400

    Time: 41 ms (48.90%) | Memory: 18 MB (8.52%) - LeetSync

--- 104-maximum-depth-of-binary-tree/maximum-depth-of-binary-tree.py
+++ 104-maximum-depth-of-binary-tree/maximum-depth-of-binary-tree.py
@@ -16,15 +16,37 @@ class Solution:
 
         # ------------------------
         # # Method 2: Iteration with Stack
-        st = []
-        if root is not None:
-            st.append((1, root))
-        depth = 0
-        while st:
-            current_depth, root = st.pop()
-            if root is not None:
-                depth = max(depth, current_depth)
-                st.append((current_depth+1, root.left))
-                st.append((current_depth+1, root.right))
-        return depth
+        # st = []
+        # if root is not None:
+        #     st.append((1, root))
+        # depth = 0
+        # while st:
+        #     current_depth, root = st.pop()
+        #     if root is not None:
+        #         depth = max(depth, current_depth)
+        #         st.append((current_depth+1, root.left))
+        #         st.append((current_depth+1, root.right))
+        # return depth
 
+        # Method 3: DFS and Tail Recursion
+        if not root:
+            return 0
+        self.next_items = []
+        self.max_depth = 0
+        self.next_items.append((root, 0))
+        return self.next_maxDepth()
+    def __init__(self):
+        self.next_items = []
+        self.max_depth = 0
+    def next_maxDepth(self):
+        if not self.next_items:
+            return self.max_depth
+        next_node, next_level = self.next_items.pop(0)
+        next_level +=1
+        self.max_depth = max(next_level, self.max_depth)
+        if next_node.left:
+            self.next_items.append((next_node.left, next_level))
+        if next_node.right:
+            self.next_items.append((next_node.right, next_level))
+        return self.next_maxDepth()
+    
