3d59cb9501c11a9242ecb88828370147621de5b4
Yanlin Chen <32689753+Ychen463@users.noreply.github.com>
2024-06-03 23:28:53 -0400

    Time: 140 ms (34.03%) | Memory: 31.5 MB (22.37%) - LeetSync

--- 1544-count-good-nodes-in-binary-tree/count-good-nodes-in-binary-tree.py
+++ 1544-count-good-nodes-in-binary-tree/count-good-nodes-in-binary-tree.py
@@ -5,16 +5,30 @@
 #         self.left = left
 #         self.right = right
 class Solution:
+    # Method 1: DFS + Recursion
+    # def goodNodes(self, root: TreeNode) -> int:
+    #     return self.dfs(root, root.val)
+    # def dfs(self, node, maxValue):
+    #     if node is None:
+    #         return 0
+    #     goods = 1 if node.val >= maxValue else 0
+    #     maxValue = max(node.val,maxValue)
+    #     goods += self.dfs(node.left,maxValue)
+    #     goods += self.dfs(node.right,maxValue)
+    #     return goods
+    # ========================================
+    # Method 2: iterative + DFS
     def goodNodes(self, root: TreeNode) -> int:
-        return self.dfs(root, root.val)
-    def dfs(self, node, maxValue):
-        if node is None:
-            return 0
-        goods = 1 if node.val >= maxValue else 0
-        maxValue = max(node.val,maxValue)
-        goods += self.dfs(node.left,maxValue)
-        goods += self.dfs(node.right,maxValue)
-        return goods
-
-
-        
+        st = [(root, float("-inf"))]
+        goodNums = 0
+        while st:
+            node, maxValue = st.pop()
+            if maxValue <= node.val:
+                goodNums += 1
+            if node == None:
+                return currentGoodNums
+            if node.left:
+                st.append((node.left, max(maxValue, node.val)))
+            if node.right:
+                st.append((node.right, max(maxValue, node.val)))
+        return goodNums
