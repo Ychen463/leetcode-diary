3b69733f5b90d06d6571fdde6b282c6a0ca1285f
Yanlin Chen <32689753+Ychen463@users.noreply.github.com>
2024-06-05 00:37:02 -0400

    Time: 51 ms (51.82%) | Memory: 20.1 MB (95.52%) - LeetSync

--- 236-lowest-common-ancestor-of-a-binary-tree/lowest-common-ancestor-of-a-binary-tree.py
+++ 236-lowest-common-ancestor-of-a-binary-tree/lowest-common-ancestor-of-a-binary-tree.py
@@ -33,21 +33,47 @@
 
 #         return lca
 # ==================================================================
+# Method 2: Recursive Approach
+# class Solution:
+#     def __init__(self):
+#         # Variable to store LCA node.
+#         self.ans = None
+#     def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
+#         def recurse_tree(currentNode: TreeNode) -> bool:
+#             if not currentNode:
+#                 return False
+#             left = recurse_tree(currentNode.left)
+#             right = recurse_tree(currentNode.right)
 
+#             mid = currentNode == p or currentNode == q
+#             if int(mid) + int(left) + int(right) >=2:
+#                 self.ans = currentNode
+#             return mid or left or right
+#         recurse_tree(root)
+#         return self.ans
+
+# ==================================================================
+# Method 3: Iterative using parent pointers
 class Solution:
-    def __init__(self):
-        # Variable to store LCA node.
-        self.ans = None
     def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
-        def recurse_tree(currentNode: TreeNode) -> bool:
-            if not currentNode:
-                return False
-            left = recurse_tree(currentNode.left)
-            right = recurse_tree(currentNode.right)
+        stack = [root]
+        parent = {root:None}
+        while p not in parent or q not in parent:
+            node = stack.pop()
+            if node.left:
+                parent[node.left] = node # 记录parent
+                stack.append(node.left)
+            if node.right:
+                parent[node.right] = node # 记录parent
+                stack.append(node.right)
+         # 存储 p 的所有祖先
+        p_ancestors = set()
+        while p:
+            p_ancestors.add(p)
+            p = parent[p]
 
-            mid = currentNode == p or currentNode == q
-            if int(mid) + int(left) + int(right) >=2:
-                self.ans = currentNode
-            return mid or left or right
-        recurse_tree(root)
-        return self.ans
+        # 找到 q 的第一个公共祖先
+        while q not in p_ancestors:
+            q = parent[q]
+
+        return q
