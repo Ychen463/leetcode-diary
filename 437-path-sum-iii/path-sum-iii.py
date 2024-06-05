0e46abac58216a38ed968bf1f5a839d8875b4733
Yanlin Chen <32689753+Ychen463@users.noreply.github.com>
2024-06-04 20:08:12 -0400

    Time: 56 ms (47.83%) | Memory: 17 MB (34.88%) - LeetSync

--- 437-path-sum-iii/path-sum-iii.py
+++ 437-path-sum-iii/path-sum-iii.py
@@ -9,33 +9,49 @@
 from collections import defaultdict
 class Solution:
     def pathSum(self, root: Optional[TreeNode], targetSum: int) -> int:
-        def dfs(node: Optional[TreeNode], curr_sum: int) -> None:
+        def dfs(node, currSum):
             nonlocal count
-            if not node:
+            if node is None:
                 return
-            
-            # 更新当前前缀和
-            curr_sum += node.val
-            
-            # 检查当前前缀和减去目标值是否存在于哈希表中
-            count += prefix_sums[curr_sum - targetSum]
-            
-            # 更新哈希表中当前前缀和的计数
-            prefix_sums[curr_sum] += 1
-            
-            # 递归处理左子树
-            dfs(node.left, curr_sum)
-            # 递归处理右子树
-            dfs(node.right, curr_sum)
-            
-            # 递归结束后，恢复哈希表中当前前缀和的计数
-            prefix_sums[curr_sum] -= 1
-        
+            currSum += node.val
+            count += prefixSums[currSum - targetSum]
+            prefixSums[currSum] +=1
+            dfs(node.left, currSum)
+            dfs(node.right, currSum)
+
+            prefixSums[currSum] -= 1
+
         count = 0
-        prefix_sums = defaultdict(int)
-        prefix_sums[0] = 1  # 初始化前缀和为0的情况
-        
+        prefixSums = defaultdict(int)
+        prefixSums[0] = 1
         dfs(root, 0)
         return count
+
+
+
+
+        # def dfs(node: Optional[TreeNode], curr_sum: int) -> None:
+        #     nonlocal count
+        #     if not node:
+        #         return        
+        #     # 更新当前前缀和
+        #     curr_sum += node.val  
+        #     # 检查当前前缀和减去目标值是否存在于哈希表中
+        #     count += prefix_sums[curr_sum - targetSum]
+        #     # 更新哈希表中当前前缀和的计数
+        #     prefix_sums[curr_sum] += 1
+            
+        #     dfs(node.left, curr_sum)# 递归处理左子树
+        #     dfs(node.right, curr_sum)# 递归处理右子树
+
+        #     # 递归结束后，恢复哈希表中当前前缀和的计数
+        #     prefix_sums[curr_sum] -= 1
+        
+        # count = 0 # 用于记录满足条件的路径数量。
+        # prefix_sums = defaultdict(int) # 用于记录前缀和及其出现的次数。
+        # prefix_sums[0] = 1  # 初始化前缀和为0的情况
+        
+        # dfs(root, 0)
+        # return count
 
         
