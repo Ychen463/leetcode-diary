e6b430adebf78b8d67a1af1f14040e3cf7421752
Yanlin Chen <32689753+Ychen463@users.noreply.github.com>
2024-06-05 17:45:34 -0400

    Time: 178 ms (12.68%) | Memory: 19.9 MB (85.65%) - LeetSync

--- 1116-maximum-level-sum-of-a-binary-tree/maximum-level-sum-of-a-binary-tree.py
+++ 1116-maximum-level-sum-of-a-binary-tree/maximum-level-sum-of-a-binary-tree.py
@@ -5,25 +5,44 @@
 #         self.left = left
 #         self.right = right
 
-# Method 1: DFS
+# # Method 1: BFS
+# from collections import deque
+# class Solution:
+#     def maxLevelSum(self, root: Optional[TreeNode]) -> int:
+#         if root is None:
+#             return []
+#         queue = deque([root])
+#         res_dict = defaultdict(int)
+#         level_num = 0
+#         while queue:
+#             level = []
+#             for _ in range(len(queue)):
+#                 node = queue.popleft()
+#                 level.append(node.val)
+#                 if node.left:
+#                     queue.append(node.left)
+#                 if node.right:
+#                     queue.append(node.right)
+#             level_num += 1
+#             res_dict[level_num] = sum(level)
+        
+#         return max(res_dict, key =lambda x: res_dict[x])      
+
+# =======
+# # Method 2: DFS
 from collections import deque
 class Solution:
     def maxLevelSum(self, root: Optional[TreeNode]) -> int:
         if root is None:
             return []
-        queue = deque([root])
         res_dict = defaultdict(int)
-        level_num = 0
-        while queue:
-            level = []
-            for _ in range(len(queue)):
-                node = queue.popleft()
-                level.append(node.val)
-                if node.left:
-                    queue.append(node.left)
-                if node.right:
-                    queue.append(node.right)
-            level_num += 1
-            res_dict[level_num] = sum(level)
         
-        return max(res_dict, key =lambda x: res_dict[x])      
+        def helper( node, level):
+            if not node:
+                return
+            res_dict[level] += node.val
+            helper(node.left, level+1)
+            helper(node.right, level+1)
+        helper(root,0)
+        print(res_dict)
+        return max(res_dict, key = lambda x: res_dict[x]) +1
