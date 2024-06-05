66fdebe13f84a243dd12d7934070b5bb8e327eb1
Yanlin Chen <32689753+Ychen463@users.noreply.github.com>
2024-06-04 23:50:41 -0400

    Time: 39 ms (96.52%) | Memory: 21.1 MB (45.59%) - LeetSync

--- 236-lowest-common-ancestor-of-a-binary-tree/lowest-common-ancestor-of-a-binary-tree.py
+++ 236-lowest-common-ancestor-of-a-binary-tree/lowest-common-ancestor-of-a-binary-tree.py
@@ -5,29 +5,49 @@
 #         self.left = None
 #         self.right = None
 
+# Method 1, 2 lists
+# class Solution:
+#     def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
+#         def find_ancestors(node, target, path):
+#             if not node:
+#                 return False
+#             path.append(node)
+#             if node == target:
+#                 return True
+#             if find_ancestors(node.left, target, path) or find_ancestors(node.right, target, path):
+#                 return True
+#             path.pop()
+#             return False
+#         p_ancestors = []
+#         q_ancestors = []
+
+#         find_ancestors(root, p, p_ancestors)
+#         find_ancestors(root, q, q_ancestors)
+
+#         lca = None
+#         for u,v in zip(p_ancestors,q_ancestors):
+#             if u == v:
+#                 lca= u
+#             else:
+#                 break
+
+#         return lca
+# ==================================================================
+
 class Solution:
+    def __init__(self):
+        # Variable to store LCA node.
+        self.ans = None
     def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
-        def find_ancestors(node, target, path):
-            if not node:
+        def recurse_tree(currentNode: TreeNode) -> bool:
+            if not currentNode:
                 return False
-            path.append(node)
-            if node == target:
-                return True
-            if find_ancestors(node.left, target, path) or find_ancestors(node.right, target, path):
-                return True
-            path.pop()
-            return False
-        p_ancestors = []
-        q_ancestors = []
+            left = recurse_tree(currentNode.left)
+            right = recurse_tree(currentNode.right)
 
-        find_ancestors(root, p, p_ancestors)
-        find_ancestors(root, q, q_ancestors)
-
-        lca = None
-        for u,v in zip(p_ancestors,q_ancestors):
-            if u == v:
-                lca= u
-            else:
-                break
-
-        return lca
+            mid = currentNode == p or currentNode == q
+            if int(mid) + int(left) + int(right) >=2:
+                self.ans = currentNode
+            return mid or left or right
+        recurse_tree(root)
+        return self.ans
