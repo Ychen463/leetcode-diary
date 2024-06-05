90e75fc0872ce41819b05587864841ec6dfc077c
Yanlin Chen <32689753+Ychen463@users.noreply.github.com>
2024-06-05 16:43:36 -0400

    Time: 42 ms (17.10%) | Memory: 16.5 MB (39.90%) - LeetSync

--- 199-binary-tree-right-side-view/binary-tree-right-side-view.py
+++ 199-binary-tree-right-side-view/binary-tree-right-side-view.py
@@ -6,23 +6,47 @@
 #         self.right = right
 
 # Method 1: BFS
+# from collections import deque
+# class Solution:
+#     def rightSideView(self, root: Optional[TreeNode]) -> List[int]:
+#         if not root:
+#             return []
+#         queue = deque([root])
+#         result = []
+
+#         while queue:
+#             level = []
+#             for _ in range(len(queue)):
+#                 node = queue.popleft()
+#                 level.append(node.val)
+#                 if node.left:
+#                     queue.append(node.left)
+#                 if node.right:
+#                     queue.append(node.right)
+
+#             result.append(level[-1])
+#         return result
+
+# # Method 1: BFS: Two Queues
 from collections import deque
 class Solution:
-    def rightSideView(self, root: Optional[TreeNode]) -> List[int]:
-        if not root:
+    def rightSideView(self, root: Optional[TreeNode]) -> List[int]: 
+        if root is None:
             return []
-        queue = deque([root])
-        result = []
+        rightside = []
+        nextLevel = deque([root])
 
-        while queue:
-            level = []
-            for _ in range(len(queue)):
-                node = queue.popleft()
-                level.append(node.val)
-                if node.left:
-                    queue.append(node.left)
-                if node.right:
-                    queue.append(node.right)
+        while nextLevel:
+            currLevel = nextLevel
+            nextLevel = deque()
 
-            result.append(level[-1])
-        return result
+            while currLevel:
+                node = currLevel.popleft()
+                if node.left: # First one must be Left Child
+                    nextLevel.append(node.left)
+                if node.right: # Second one must be Left Child
+                    nextLevel.append(node.right)
+                # So the last would be the last right child
+            rightside.append(node.val)
+        return rightside
+            
