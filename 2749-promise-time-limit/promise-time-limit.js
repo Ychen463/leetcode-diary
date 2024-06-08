6040df0d4a12633f9b9e1ac05de6f23ea90a92b7
Yanlin Chen <32689753+Ychen463@users.noreply.github.com>
2024-06-08 16:29:47 -0400

    Time: 56 ms (75.98%) | Memory: 49.3 MB (68.64%) - LeetSync

--- 2749-promise-time-limit/promise-time-limit.js
+++ 2749-promise-time-limit/promise-time-limit.js
@@ -3,15 +3,29 @@
  * @param {number} t
  * @return {Function}
  */
+// var timeLimit = function(fn, t) {
+    
+//     return async function(...args) {
+//         return new Promise((res, rej)=>{
+//             const timeOutId = setTimeout(()=>{
+//                 rej("Time Limit Exceeded");
+//             },t);
+//         fn(...args).then(res).catch(rej).finally(()=>{clearTimeout(timeOutId)});
+//         })
+//     }
+// };
+
+// Approach 3: Promise Race
 var timeLimit = function(fn, t) {
     
     return async function(...args) {
-        return new Promise((res, rej)=>{
-            const timeOutId = setTimeout(()=>{
-                rej("Time Limit Exceeded");
-            },t);
-        fn(...args).then(res).catch(rej).finally(()=>{clearTimeout(timeOutId)});
-        })
+        const timeLimitPromise = new Promise((res, rej) => {
+            setTimeout(()=>{
+                rej("Time Limit Exceeded")
+            },t)
+        });
+        const returnedPromise = fn(...args);
+        return Promise.race([timeLimitPromise,returnedPromise]);
     }
 };
 
