150bb3e8689acbd18e54a7dc0024dfd1d4edcf2a
Yanlin Chen <32689753+Ychen463@users.noreply.github.com>
2024-06-08 15:47:50 -0400

    Time: 928 ms (5.05%) | Memory: 36.1 MB (84.09%) - LeetSync

--- 523-continuous-subarray-sum/continuous-subarray-sum.py
+++ 523-continuous-subarray-sum/continuous-subarray-sum.py
@@ -1,18 +1,17 @@
 class Solution:
     def checkSubarraySum(self, nums: List[int], k: int) -> bool:
         
-        prefix_sum = 0
-        prefix_sum_mod = {0:-1}
+        prefix_mod = 0
+        mod_seen = {0:-1}
 
         for i in range(len(nums)):
-            prefix_sum += nums[i]
-            if k != 0:
-                prefix_sum %= k # 余数
-            if prefix_sum in prefix_sum_mod:
-                if i-prefix_sum_mod[prefix_sum] >=2:
+            prefix_mod = (prefix_mod + nums[i]) % k
+
+            if prefix_mod in mod_seen:
+                if i-mod_seen[prefix_mod] >=2:
                     return True
             else:
-                prefix_sum_mod[prefix_sum] = i
+                mod_seen[prefix_mod] = i
         return False
             
             
