604270765991b3e2b881bb75da8794a21cdbf738
Yanlin Chen <32689753+Ychen463@users.noreply.github.com>
2024-06-10 15:50:21 -0400

    Time: 60 ms (5.17%) | Memory: 16.2 MB (99.75%) - LeetSync

--- 1137-height-checker/height-checker.py
+++ 1137-height-checker/height-checker.py
@@ -1,8 +1,33 @@
+# class Solution:
+#     def heightChecker(self, heights: List[int]) -> int:
+#         expected = sorted(heights)
+#         counter = 0
+#         for i in range(len(heights)):
+#             if heights[i] != expected[i]:
+#                 counter+=1
+#         return counter
+
+# ================ Method 1: Bubble Sort ===================
 class Solution:
     def heightChecker(self, heights: List[int]) -> int:
-        expected = sorted(heights)
-        counter = 0
-        for i in range(len(heights)):
-            if heights[i] != expected[i]:
-                counter+=1
-        return counter
+        print(heights)
+        def bubble_sort():
+            n = len(sorted_heights)
+            # Loop through the array for bubble sort passes.
+            for i in range(n - 1):
+                # Inner loop to compare and swap elements.
+                for j in range(n - i - 1):
+                    # Compare and swap if elements are in the wrong order.
+                    if sorted_heights[j] > sorted_heights[j + 1]:
+                        sorted_heights[j], sorted_heights[j + 1] = (
+                            sorted_heights[j + 1],
+                            sorted_heights[j],
+                        )
+        sorted_heights = heights[:]
+        bubble_sort()
+        count = 0
+        for i in range(len(sorted_heights)):
+            if heights[i] != sorted_heights[i]:
+                count +=1
+        return count
+        
