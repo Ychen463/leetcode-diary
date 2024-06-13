e5629b467e2e15d7a684feb2668a1563583e8fc6
Yanlin Chen <32689753+Ychen463@users.noreply.github.com>
2024-06-13 16:53:57 -0400

    Time: 55 ms (82.16%) | Memory: 16.5 MB (38.97%) - LeetSync

--- 2148-minimum-number-of-moves-to-seat-everyone/minimum-number-of-moves-to-seat-everyone.py
+++ 2148-minimum-number-of-moves-to-seat-everyone/minimum-number-of-moves-to-seat-everyone.py
@@ -1,10 +1,28 @@
+# # ============ Approach 1: Sorting (Greedy) ============
+# class Solution:
+#     def minMovesToSeat(self, seats: List[int], students: List[int]) -> int:
+#         seats.sort()
+#         students.sort()
+#         count =0 
+#         for i in range(len(seats)):
+#             ab = abs(seats[i] - students[i])
+#             count += ab
+#         return count
+
+# ============ Approach 2: Counting Sort ============
 class Solution:
     def minMovesToSeat(self, seats: List[int], students: List[int]) -> int:
-        seats.sort()
-        students.sort()
-        count =0 
+        maxValue = max(max(seats),max(students) )
+        differences = [0] * maxValue
+
         for i in range(len(seats)):
-            ab = abs(seats[i] - students[i])
-            count += ab
-        return count
+            differences[seats[i] - 1] += 1
+        for i in range(len(students)):
+            differences[students[i] - 1] -= 1
+        moves = 0
+        unmatched = 0
+        for difference in differences:
+            moves += abs(unmatched)
+            unmatched += difference
+        return moves
         
