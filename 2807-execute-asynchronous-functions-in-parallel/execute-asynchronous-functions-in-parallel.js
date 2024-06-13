4d2a6e62216bc1588af5280e794817a31cabdedf
Yanlin Chen <32689753+Ychen463@users.noreply.github.com>
2024-06-13 17:58:50 -0400

    Time: 56 ms (98.15%) | Memory: 49 MB (95.97%) - LeetSync

--- 2807-execute-asynchronous-functions-in-parallel/execute-asynchronous-functions-in-parallel.js
+++ 2807-execute-asynchronous-functions-in-parallel/execute-asynchronous-functions-in-parallel.js
@@ -2,29 +2,52 @@
  * @param {Array<Function>} functions
  * @return {Promise<any>}
  */
+ // Implementation 1: Using async/await Syntax
+// var promiseAll = function(functions) {
+//     return new Promise((res,rej) => {
+//         if (functions.length == 0){
+//             res([]);
+//             return;
+//         }
+//         const newArr = Array(functions.length).fill(null);
+//         let resolvedCount = 0;
+
+//         functions.forEach( async (el, idx) =>{
+//             try{
+//                 const subResult= await el();
+//                 newArr[idx] = subResult;
+//                 resolvedCount++;
+//                 if (resolvedCount == functions.length){
+//                     res(newArr);
+//                 }
+//             } catch(err){
+//                 rej(err);
+//             }
+//         })
+//     })
+// };
+
 var promiseAll = function(functions) {
-    return new Promise((res,rej) => {
+    return new Promise((res, rej) => {
         if (functions.length == 0){
             res([]);
             return;
         }
+        let resolvedCount =0;
         const newArr = Array(functions.length).fill(null);
-        let resolvedCount = 0;
-
-        functions.forEach( async (el, idx) =>{
-            try{
-                const subResult= await el();
+        functions.forEach( (el,idx) =>{
+            el().then((subResult)=>{
                 newArr[idx] = subResult;
                 resolvedCount++;
-                if (resolvedCount == functions.length){
+                if (functions.length == resolvedCount){
                     res(newArr);
                 }
-            } catch(err){
+            }).catch((err)=>{
                 rej(err);
-            }
+            })
         })
     })
-};
+}
 
 /**
  * const promise = promiseAll([() => new Promise(res => res(42))])
