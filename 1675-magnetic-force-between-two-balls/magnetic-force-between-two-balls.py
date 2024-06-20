011ddbe0a52b2ce290d24eb1afc4ef1f021bbb42
Yanlin Chen <32689753+Ychen463@users.noreply.github.com>
2024-06-20 16:34:18 -0400

    Time: 936 ms (39.43%) | Memory: 30.2 MB (67.61%) - LeetSync

--- 1675-magnetic-force-between-two-balls/magnetic-force-between-two-balls.py
+++ 1675-magnetic-force-between-two-balls/magnetic-force-between-two-balls.py
@@ -9,18 +9,20 @@ class Solution:
             mid = low + (high - low) //2
             if self.canPlaceBall(mid, position, m):
                 answer = mid
-                low = mid + 1
+                low = mid + 1 # 如果可以，试图增加距离
             else:
                 high = mid -1
         return answer
 
-
-
     def canPlaceBall(self, x, position, m):
-        prev_ball_pos = position[0]
+        #这个函数用于检查在给定距离 x 下是否可以放置 m 个球。
+
+        prev_ball_pos = position[0] #初始放置第一个球在 position[0]。
         balls_placed = 1
         for i in range( 1, len(position)):
             curr_pos = position[i]
+            # 遍历数组，如果当前位置与上一个放置球的位置之间的距离大于等于 x，
+            # 则放置一个球，并更新上一个球的位置。
             if curr_pos - prev_ball_pos >= x:
                 balls_placed +=1
                 prev_ball_pos = curr_pos
