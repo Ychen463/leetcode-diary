a6e258d67380329b1419e48e4e46d758d551f86a
Yanlin Chen <32689753+Ychen463@users.noreply.github.com>
2024-06-20 17:07:30 -0400

    Time: 35 ms (99.53%) | Memory: 49.3 MB (41.63%) - LeetSync

--- 2864-is-object-empty/is-object-empty.js
+++ 2864-is-object-empty/is-object-empty.js
@@ -7,9 +7,12 @@
 //     if (JSON.stringify(obj).length <= 2) return true
 //     else return false
 // };
-// =========== Approach 2: Using Object.keys ===========
-var isEmpty = function(obj) {
-    return Object.keys(obj).length == 0
-}
-// // =========== Approach 3: Using loop ===========
+// // =========== Approach 2: Using Object.keys ===========
 // var isEmpty = function(obj) {
+//     return Object.keys(obj).length == 0
+// }
+// =========== Approach 3: Using loop ===========
+var isEmpty = function(obj) {
+    for (const _ in obj) return false;
+    return true;
+}
