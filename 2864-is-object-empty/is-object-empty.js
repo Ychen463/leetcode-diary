469a44ad990a4013786152c255396e6b663df2c3
Yanlin Chen <32689753+Ychen463@users.noreply.github.com>
2024-06-20 17:06:59 -0400

    Time: 44 ms (91.26%) | Memory: 49.2 MB (54.73%) - LeetSync

--- 2864-is-object-empty/is-object-empty.js
+++ 2864-is-object-empty/is-object-empty.js
@@ -2,7 +2,14 @@
  * @param {Object|Array} obj
  * @return {boolean}
  */
+// // =========== Approach 1: Using JSON.stringify ===========
+// var isEmpty = function(obj) {
+//     if (JSON.stringify(obj).length <= 2) return true
+//     else return false
+// };
+// =========== Approach 2: Using Object.keys ===========
 var isEmpty = function(obj) {
-    if (JSON.stringify(obj).length <= 2) return true
-    else return false
-};
+    return Object.keys(obj).length == 0
+}
+// // =========== Approach 3: Using loop ===========
+// var isEmpty = function(obj) {
