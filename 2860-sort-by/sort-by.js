ca84b58956de23c998f18da937e71dae4f872c3b
Yanlin Chen <32689753+Ychen463@users.noreply.github.com>
2024-06-24 16:01:21 -0400

    Time: 141 ms (11.75%) | Memory: 66.4 MB (46.07%) - LeetSync

--- 2860-sort-by/sort-by.js
+++ 2860-sort-by/sort-by.js
@@ -3,6 +3,14 @@
  * @param {Function} fn
  * @return {Array}
  */
+//  // ========== Implementation 1: Subtraction-Based Comparator ============
+// var sortBy = function(arr, fn) {
+//     return arr.sort((a,b)=>fn(a)-fn(b));
+// };
+ // ========== Implementation 2: Comparison-Based Comparator ============
 var sortBy = function(arr, fn) {
-    return arr.sort((a,b)=>fn(a)-fn(b));
+    var swap = (a, b)=>{
+        return (fn(a)<fn(b))? -1 : 1
+    }
+    return arr.sort(swap)
 };
