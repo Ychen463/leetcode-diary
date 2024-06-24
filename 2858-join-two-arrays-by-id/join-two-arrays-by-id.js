9ca4faaa4f7b85cc3fe000987cbb45797f7a95a2
Yanlin Chen <32689753+Ychen463@users.noreply.github.com>
2024-06-24 16:55:10 -0400

    Time: 393 ms (24.74%) | Memory: 96.6 MB (30.21%) - LeetSync

--- 2858-join-two-arrays-by-id/join-two-arrays-by-id.js
+++ 2858-join-two-arrays-by-id/join-two-arrays-by-id.js
@@ -20,20 +20,57 @@
 //     return returnArr
 // };
 
- // =========== Approach 2: Using Map =========== 
+//  // =========== Approach 2: Using Map =========== 
+// var join = function(arr1, arr2) {
+//     const map = new Map();
+//     for (const obj of arr1) map.set(obj.id, obj);
+//     for (const obj of arr2) {
+//         if (!map.has(obj.id)) map.set(obj.id, obj);
+//         else {
+//             const prevObj = map.get(obj.id);
+//             for (const key of Object.keys(obj)){
+//                 prevObj[key] = obj[key];
+//             }
+//         }
+//     }
+//     const res = new Array();
+//     for (const key of map.keys()) res.push(map.get(key));
+//     return res.sort((a,b)=>(a.id - b.id))
+// }
+
+ // =========== Approach 3: Using Two pointers =========== 
 var join = function(arr1, arr2) {
-    const map = new Map();
-    for (const obj of arr1) map.set(obj.id, obj);
-    for (const obj of arr2) {
-        if (!map.has(obj.id)) map.set(obj.id, obj);
-        else {
-            const prevObj = map.get(obj.id);
-            for (const key of Object.keys(obj)){
-                prevObj[key] = obj[key];
-            }
+    arr1.sort((a,b)=>(a.id - b.id));
+    arr2.sort((a,b)=>(a.id - b.id));
+    let i = 0;
+    let j = 0;
+    const combinedArray = [];
+
+    while (i < arr1.length && j < arr2.length) {
+        if (arr1[i].id === arr2[j].id) {
+            combinedArray.push({...arr1[i], ...arr2[j]})
+            i++;
+            j++;
+            continue
+        }
+        if (arr1[i].id < arr2[j].id) {
+            combinedArray.push({...arr1[i]})
+            i++
+            continue
+        } 
+        if (arr1[i].id > arr2[j].id) {
+            combinedArray.push({...arr2[j]});
+            j++;
+            continue
         }
     }
-    const res = new Array();
-    for (const key of map.keys()) res.push(map.get(key));
-    return res.sort((a,b)=>(a.id - b.id))
+    while (i < arr1.length){
+        combinedArray.push({...arr1[i]})
+        i++
+    }
+    while (j < arr2.length){
+        combinedArray.push({...arr2[j]})
+        j++
+    }
+    return combinedArray
 }
