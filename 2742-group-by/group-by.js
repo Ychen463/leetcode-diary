3e5e0fb3f45edeaba0525891a7d48b71f6421d2b
Yanlin Chen <32689753+Ychen463@users.noreply.github.com>
2024-06-24 15:47:28 -0400

    Time: 120 ms (71.98%) | Memory: 73.2 MB (38.53%) - LeetSync

--- 2742-group-by/group-by.js
+++ 2742-group-by/group-by.js
@@ -2,19 +2,29 @@
  * @param {Function} fn
  * @return {Object}
  */
+//  // ========== Approach 1: For Loop ==================
+// Array.prototype.groupBy = function(fn) {
+//     const returnObject = {};
+//     for (const each of this){
+//         const key = fn(each);
+//         if (key in returnObject){
+//             returnObject[key].push(each);
+//         }else{
+//             returnObject[key] = [each]
+//         }
+//     }
+//     return returnObject
+// };
+// ====== Approach 2: Reduce  ==================
 Array.prototype.groupBy = function(fn) {
-    const returnObject = {};
+    return this.reduce((accum, item)=>{
+        const key = fn(item);
+        accum[key] ||= [];
+        accum[key].push(item);
+        return accum;
+    },{})
+}
 
-    for (const each of this){
-        const key = fn(each);
-        if (key in returnObject){
-            returnObject[key].push(each);
-        }else{
-            returnObject[key] = [each]
-        }
-    }
-    return returnObject
-};
 
 /**
  * [1,2,3].groupBy(String) // {"1":[1],"2":[2],"3":[3]}
