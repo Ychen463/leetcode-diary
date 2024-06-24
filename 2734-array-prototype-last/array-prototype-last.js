a5b5131cbffea1f1c0c7f5100ec760c11306cf60
Yanlin Chen <32689753+Ychen463@users.noreply.github.com>
2024-06-24 15:30:46 -0400

    Time: 50 ms (60.49%) | Memory: 48.5 MB (77.48%) - LeetSync

--- 2734-array-prototype-last/array-prototype-last.js
+++ 2734-array-prototype-last/array-prototype-last.js
@@ -1,11 +1,17 @@
 /**
  * @return {null|boolean|number|string|Array|Object}
  */
+ // ========= Approach 1: Extending Array Prototype to Include a .last() Method
 Array.prototype.last = function() {
     if (this.length === 0){
         return -1
     } 
     return  this[this.length -1] 
+};
+ // ========= Approach 1: Extending Array Prototype to Include a .last() Method
+Array.prototype.last = function() {
+    
+    return  this.length === 0? -1 : this[this.length -1] 
 };
 
 /**
