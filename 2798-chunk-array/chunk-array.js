3ff3273f5689f05ab9ab4a91cb15533594070f60
Yanlin Chen <32689753+Ychen463@users.noreply.github.com>
2024-06-24 15:05:47 -0400

    Time: 70 ms (6.51%) | Memory: 53.5 MB (5.09%) - LeetSync

--- 2798-chunk-array/chunk-array.js
+++ 2798-chunk-array/chunk-array.js
@@ -3,12 +3,41 @@
  * @param {number} size
  * @return {Array}
  */
+//  // ====== Approach 2: Using Slicing ====== 
+// var chunk = function(arr, size) {
+//     let i = 0;
+//     const new_arr = []
+//     while ( i < arr.length) {
+//         new_arr.push(arr.slice(i, i + size))
+//         i = i+size 
+//     }
+//     return new_arr
+// };
+
+// // ======= Approach 4: Using Reduce ======= 
+// var chunk = function(arr, size) {
+//     return arr.reduce((chuckedArr, element)=>{
+//         const lastChuck = chuckedArr[chuckedArr.length - 1];
+//         if (!lastChuck || lastChuck.length === size) {
+//             chuckedArr.push([element]);
+//         } else{
+//             lastChuck.push(element);
+//         }
+//         return chuckedArr
+//     },[])
+// }
+
+// ======= Approach 5: Using Push ======= 
 var chunk = function(arr, size) {
-    let i = 0;
-    new_arr = []
-    while ( i < arr.length) {
-        new_arr.push(arr.slice(i, i + size))
-        i = i+size 
+    let currChunck = []
+    const result = []
+    for (element of arr){
+        if (currChunck.length === size){
+            result.push(currChunck);
+            currChunck = [];
+        }
+        currChunck.push(element);
     }
-    return new_arr
-};
+    if (currChunck.length) result.push(currChunck);
+    return result
+}
