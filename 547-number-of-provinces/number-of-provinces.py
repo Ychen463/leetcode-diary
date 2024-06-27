b7f095a75ca1d1df8ce720297bf3befeb50ce40d
Yanlin Chen <32689753+Ychen463@users.noreply.github.com>
2024-06-27 16:59:37 -0400

    Time: 184 ms (54.30%) | Memory: 17.2 MB (90.78%) - LeetSync

--- 547-number-of-provinces/number-of-provinces.py
+++ 547-number-of-provinces/number-of-provinces.py
@@ -1,21 +1,39 @@
-# ========= DFS ========
+# # ================== Approach 1: Depth First Search =================
+# class Solution:
+#     def findCircleNum(self, isConnected: List[List[int]]) -> int:
+#         def dfs(node):
+#             # 只访问直接相连且未访问过的节点: [1,1,0]
+#             for neighbor, is_connected in enumerate(isConnected[node]):
+#                 if neighbor not in visited and is_connected:
+#                     visited.add(neighbor)
+#                     dfs(neighbor)
+#         visited = set()
+#         count = 0
+#         for i in range(len(isConnected)):
+#             if i not in visited:
+#                 visited.add(i)
+#                 dfs(i)
+#                 count +=1
+#         return count
+
+# ================== Approach 2: Breadth First Search ==================
+from collections import deque
+
 class Solution:
     def findCircleNum(self, isConnected: List[List[int]]) -> int:
-        
-        def dfs(node):
-            for neighbor, is_connected in enumerate(isConnected[node]):
-                if neighbor not in visited and is_connected:
-                    visited.add(neighbor)
-                    dfs(neighbor)
-
+        def bfs(node):
+            queue = deque([node])
+            while queue:
+                node = queue.popleft()
+                for neighbor, is_connected in enumerate(isConnected[node]):
+                    if neighbor not in visited and is_connected:
+                        queue.append(neighbor)
+                        visited.add(neighbor)
         visited = set()
         count = 0
         for i in range(len(isConnected)):
             if i not in visited:
+                count+=1
+                bfs(i)
                 visited.add(i)
-                dfs(i)
-                count +=1
         return count
-
-
-        
