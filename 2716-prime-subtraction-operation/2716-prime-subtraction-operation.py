5314a16483112b4547b6f92be00cac2765264fe9
Yanlin Chen <32689753+Ychen463@users.noreply.github.com>
2024-11-11 15:49:59 -0500

    Time: 95 ms (33.15%), Space: 16.7 MB (67.8%) - LeetHub

--- 2716-prime-subtraction-operation/2716-prime-subtraction-operation.py
+++ 2716-prime-subtraction-operation/2716-prime-subtraction-operation.py
@@ -1,3 +1,4 @@
+# =============== Approach 1: Brute Force =============== 
 class Solution:
     def check_prime(self, x): # 定义一个方法用来判断一个数是否是质数
         for i in range(2, int(x**0.5) + 1): # 如果x能够被i整除，说明x不是质数
@@ -6,8 +7,8 @@ class Solution:
         # 如果没有发现可以整除x的i，则x是质数
         return True
     
+    # 主函数，用来执行质数减法操作
     def primeSubOperation(self, nums: List[int]) -> bool:
-# 主函数，用来执行质数减法操作
         for i in range(len(nums)): # 遍历数组中的每个元素
             if i == 0:
                 bound = nums[0] # 对于第一个元素，我们需要找到一个小于nums[0]的最大质数
@@ -30,10 +31,34 @@ class Solution:
         
             nums[i] = nums[i] - largest_prime # 用找到的最大质数减去当前的nums[i]
         return True        # 如果完成整个循环，说明数组可以变成严格递增，返回True
-
-
-
-        
-
-
-        
+# =============== Approach 2: Storing the primes =============== 
+class Solution:
+    def isprime(self, x):    # 定义一个方法来判断一个数是否是质数
+        for i in range(2, isqrt(x)+1): # 遍历从2到sqrt(n)的整数，如果有因数说明n不是质数
+            if x % i ==0:
+                return False
+        return True        # 如果没有找到任何因数，则n是质数
+    def primeSubOperation(self, nums):
+        maxElement = max(nums) # 找出数组中的最大元素
+        prev_primes = [0] * (maxElement + 1)
+        for i in range(2, maxElement + 1):
+            if self.isprime(i):
+                prev_primes[i] = i
+            else:
+                prev_primes[i] = prev_primes[i-1]
+        for i in range(len(nums)): # 遍历数组中的每个元素
+            if i == 0:
+                bound = nums[0]
+            else:
+            # 对于其他元素，我们需要找到一个质数，使得当前元素减去该质数后尽可能接近上一个元素
+                bound = nums[i] - nums[i - 1]
+            
+            # 如果bound小于等于0，说明无法使数组严格递增，直接返回False
+            if bound <= 0:
+                return False
+            # 使用previous_prime数组找到小于bound的最大质数
+            largest_prime = prev_primes[bound - 1]
+            # 用找到的最大质数减去当前的nums[i]
+            nums[i] -= largest_prime
+        # 如果完成整个循环，说明数组可以变成严格递增，返回True
+        return True
