b5b1de6038563ffd47f251b3c48cffd9deb570e7
Yanlin Chen <32689753+Ychen463@users.noreply.github.com>
2024-11-11 16:32:09 -0500

    Time: 92 ms (37.01%), Space: 27.8 MB (84.18%) - LeetHub

--- 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.py
+++ 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.py
@@ -1,5 +1,16 @@
+# ============== Approach 1: Sort ==============
+# class Solution:
+#     def findKthLargest(self, nums: List[int], k: int) -> int:
+#         nums.sort(reverse=True)
+#         return nums[k-1]
+
+# ============== Approach 1: Sort ==============
 class Solution:
     def findKthLargest(self, nums: List[int], k: int) -> int:
-        nums.sort(reverse=True)
-        return nums[k-1]
+        heap = []
+        for num in nums:
+            heapq.heappush(heap,num)
+            if len(heap) > k:
+                heapq.heappop(heap)
+        return heap[0]
         
