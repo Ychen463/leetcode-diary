a80d1a136eb43a646551ab131b97de546959e874
Yanlin Chen <32689753+Ychen463@users.noreply.github.com>
2024-11-11 17:23:51 -0500

    Time: 44 ms (88.13%), Space: 28.3 MB (61.17%) - LeetHub

--- 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.py
+++ 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.py
@@ -4,13 +4,34 @@
 #         nums.sort(reverse=True)
 #         return nums[k-1]
 
-# ============== Approach 1: Sort ==============
+# ============== Approach 2: Min-Heap ==============
+# class Solution:
+#     def findKthLargest(self, nums: List[int], k: int) -> int:
+#         min_heap = []
+#         for num in nums:
+#             heapq.heappush(min_heap,num)
+#             if len(min_heap) > k:
+#                 heapq.heappop(min_heap)
+#         return min_heap[0]
+# ============== Approach 3: Quickselect ==============
+import random
+
 class Solution:
     def findKthLargest(self, nums: List[int], k: int) -> int:
-        heap = []
-        for num in nums:
-            heapq.heappush(heap,num)
-            if len(heap) > k:
-                heapq.heappop(heap)
-        return heap[0]
-        
+        def quick_select(nums, k):
+            pivot = random.choice(nums)
+            left, mid, right = [], [], []
+            for num in nums:
+                if num > pivot:
+                    left.append(num)
+                elif num < pivot:
+                    right.append(num)
+                else:
+                    mid.append(num)
+            if k <= len(left):
+                return quick_select(left, k)
+            if len(left) + len(mid) <k:
+                return quick_select(right, k-len(left)-len(mid))
+            return pivot
+        return quick_select(nums, k)
+
