1fcb29bef23db027ef32939a30e5f9ed9afc7366
Yanlin Chen <32689753+Ychen463@users.noreply.github.com>
2024-11-30 12:12:11 -0500

    Time: 293 ms (7.32%), Space: 40.1 MB (27.03%) - LeetHub

---
+++ 2636-maximum-subsequence-score/2636-maximum-subsequence-score.py
@@ -0,0 +1,23 @@
+import heapq
+class Solution:
+    def maxScore(self, nums1: List[int], nums2: List[int], k: int) -> int:
+        # Step 1: 将 nums1 和 nums2 按 nums2 值降序排列
+        pairs = sorted(zip(nums2, nums1),reverse = True)
+        print(pairs)
+        
+        # Step 2: 使用一个最小堆维护 nums1 的前 k 大值
+        min_heap = []
+        current_sum = 0
+        max_score = 0
+
+        for num2, num1 in pairs:
+            heapq.heappush(min_heap, num1)
+            current_sum += num1
+
+            if len(min_heap) > k:
+                current_sum -= heapq.heappop(min_heap)
+            if len(min_heap) == k:
+                max_score = max(max_score, current_sum * num2)
+
+        return max_score
+            
