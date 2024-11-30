e2546996befd480236cfc12d61a332c6802c89c5
Yanlin Chen <32689753+Ychen463@users.noreply.github.com>
2024-11-30 09:44:02 -0500

    Time: 11 ms (91.4%), Space: 17.9 MB (5.18%) - LeetHub

--- 2413-smallest-number-in-infinite-set/2413-smallest-number-in-infinite-set.py
+++ 2413-smallest-number-in-infinite-set/2413-smallest-number-in-infinite-set.py
@@ -3,24 +3,23 @@ class SmallestInfiniteSet:
 
     def __init__(self):
         self.heap = []
-        # removed 集合实际上是用来记录 "已经被移出主集合的数字（候选集合之外）"
-        self.removed = set()
+        # added_integers 集合实际上是用来记录 "已经被移出主集合的数字（候选集合之外）"
+        self.added_integers = set()
         self.current = 1
 
     def popSmallest(self) -> int:
         if self.heap:
             smallest = heapq.heappop(self.heap)
-            self.removed.remove(smallest)
-            return smallest
-        smallest = self.current
-        self.current += 1
-
+            self.added_integers.remove(smallest)
+        else:
+            smallest = self.current
+            self.current += 1
         return smallest
 
     def addBack(self, num: int) -> None:
-        if num < self.current and num not in self.removed:
+        if num < self.current and num not in self.added_integers:
             heapq.heappush(self.heap,num)
-            self.removed.add(num)
+            self.added_integers.add(num)
 
         
 
