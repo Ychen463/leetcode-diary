4c9d534bdffb55c5c6602fcce76ad6423310e63c
Yanlin Chen <32689753+Ychen463@users.noreply.github.com>
2024-11-29 19:59:07 -0500

    Time: 19 ms (67.04%), Space: 17.9 MB (5.18%) - LeetHub

--- 2413-smallest-number-in-infinite-set/2413-smallest-number-in-infinite-set.py
+++ 2413-smallest-number-in-infinite-set/2413-smallest-number-in-infinite-set.py
@@ -1,7 +1,9 @@
 import heapq
 class SmallestInfiniteSet:
+
     def __init__(self):
         self.heap = []
+        # removed 集合实际上是用来记录 "已经被移出主集合的数字（候选集合之外）"
         self.removed = set()
         self.current = 1
 
@@ -12,15 +14,15 @@ class SmallestInfiniteSet:
             return smallest
         smallest = self.current
         self.current += 1
+
         return smallest
-        
 
     def addBack(self, num: int) -> None:
         if num < self.current and num not in self.removed:
             heapq.heappush(self.heap,num)
             self.removed.add(num)
 
-
+        
 
 
 # Your SmallestInfiniteSet object will be instantiated and called as such:
